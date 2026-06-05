1be2ebd6ce1f0eaa2e1ceb977e329367c019b781
argerioqsf <argerioaf3@gmail.com>
2026-06-05 10:05:14 -0300

    feat(decks): integrate JwtModule for authentication in DecksModule

--- apps/api/src/decks/decks.module.ts
+++ apps/api/src/decks/decks.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { PrismaModule } from "../prisma/prisma.module";
 import { DecksController } from "./decks.controller";
 import { DecksService } from "./decks.service";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule,JwtModule.register({})],
   controllers: [DecksController],
   providers: [DecksService]
 })
