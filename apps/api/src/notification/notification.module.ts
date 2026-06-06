9949e862ed49146cb6644e27f2f153d6194e8bc8
argerioqsf <argerioaf3@gmail.com>
2026-06-06 10:07:01 -0300

    feat(notification): integrate JwtModule for authentication in NotificationModule

--- apps/api/src/notification/notification.module.ts
+++ apps/api/src/notification/notification.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { NotificationController } from "./notification.controller";
 import { PrismaModule } from "../prisma/prisma.module";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, JwtModule.register({})],
   controllers: [NotificationController],
 })
 export class NotificationModule {}
